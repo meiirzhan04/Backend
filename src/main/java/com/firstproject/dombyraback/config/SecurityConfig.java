986530da26eccb49a3286fa6a22b0f9c34dbdd12
meirzhan <amirzhanmeirzhan5@gmail.com>
2026-01-22 17:05:54 +0500

    commit

--- src/main/java/com/firstproject/dombyraback/config/SecurityConfig.java
+++ src/main/java/com/firstproject/dombyraback/config/SecurityConfig.java
@@ -24,18 +24,20 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(
-                        session -> session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**"
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
                         jwtFilter,
+
                         UsernamePasswordAuthenticationFilter.class
                 );
 
