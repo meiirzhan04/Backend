19b662a3d3ec7d16fa4849ffcce43762e0f2fced
meirzhan <amirzhanmeirzhan5@gmail.com>
2026-01-22 18:42:33 +0500

    add some changes

--- src/main/java/com/firstproject/dombyraback/config/SecurityConfig.java
+++ src/main/java/com/firstproject/dombyraback/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api/users"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
