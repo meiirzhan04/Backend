eaffe044701e551d6bc039c59fedcf5f42babb3f
meirzhan <amirzhanmeirzhan5@gmail.com>
2026-01-26 17:00:55 +0500

    add some changes

--- src/main/java/com/firstproject/dombyraback/controller/AuthController.java
+++ src/main/java/com/firstproject/dombyraback/controller/AuthController.java
@@ -94,7 +94,7 @@ public class AuthController {
         boolean connected = telegramService.isUserConnected(username);
         return ResponseEntity.ok(Map.of(
                 "connected", connected,
-                "botUsername", "DombyraAuthBot"
+                "botUsername", "DombyraAppAuth"
         ));
     }
 }
